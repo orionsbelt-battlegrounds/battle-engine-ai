4476b449e9a65c2bc8b2d2854679061159c6bb0f
Pedro Santos <donbonifacio@gmail.com>
2014-03-16 17:16:35 +0000

    Add board tests

--- src/main/scala/obb/engine/actions/AllMovement.scala
+++ src/main/scala/obb/engine/actions/AllMovement.scala
@@ -15,28 +15,31 @@ class AllMovement extends TurnAction {
   def run( args : ActionArgs ) : ActionResult = {
     args.board.at(args.from) match {
       case Some(element) =>
-        if( args.board.outOfBounds(args.to) ) {
-          return ActionResult(false, args.board, 0, Some(s"OutOfBoundsCoordinate:${args.to.x},${args.to.y}"))
-        }
-        if( !args.board.adjacent(args.from, args.to) ) {
-          return ActionResult(false, args.board, 0, Some(s"NotAdjacentCoordinate:${args.to.x},${args.to.y}"))
-        }
-        var table = args.board.table - args.from
-        val quantityToMove = if(args.quantity < 0) element.quantity else args.quantity
-        val remaining = element.quantity - quantityToMove
-        var mulFactor = 1
-
-        if(remaining > 0) {
-          mulFactor = 2
-          table += (args.from -> element.forQuantity(remaining))
-        }
-        table += (args.to -> element.forQuantity(quantityToMove))
-
-        ActionResult(true, Board(args.board.sizeX, args.board.sizeY, table), element.unit.movementCost * mulFactor)
-
+        process(args, element)
       case _ =>
         ActionResult(false, args.board, 0, Some(s"EmptyCoordinate:${args.from.x},${args.from.y}"))
     }
+  }
+
+  def process(args : ActionArgs, element : Element) : ActionResult = {
+    if( args.board.outOfBounds(args.to) ) {
+      return ActionResult(false, args.board, 0, Some(s"OutOfBoundsCoordinate:${args.to.x},${args.to.y}"))
+    }
+    if( !args.board.adjacent(args.from, args.to) ) {
+      return ActionResult(false, args.board, 0, Some(s"NotAdjacentCoordinate:${args.to.x},${args.to.y}"))
+    }
+    var table = args.board.table - args.from
+    val quantityToMove = if(args.quantity < 0) element.quantity else args.quantity
+    val remaining = element.quantity - quantityToMove
+    var mulFactor = 1
+
+    if(remaining > 0) {
+      mulFactor = 2
+      table += (args.from -> element.forQuantity(remaining))
+    }
+    table += (args.to -> element.forQuantity(quantityToMove))
+
+    ActionResult(true, Board(args.board.sizeX, args.board.sizeY, table), element.unit.movementCost * mulFactor)
   }
 
 }
--- src/test/scala/obb/engine/BoardSpec.scala
+++ src/test/scala/obb/engine/BoardSpec.scala
@@ -7,6 +7,47 @@ class BoardSpec extends UnitSpec {
 
   describe("Board") {
 
+    describe("#adjacent") {
+
+      it("succeeds on adjacent squares") {
+        val board = Board.empty
+        for( x <- 1 to 3 ) {
+          for( y <- 1 to 3 ) {
+            assert( board.adjacent( Coordinate(2, 2), Coordinate(x, y)) == true )
+          }
+        }
+      }
+
+      it("fails for not adjacent squares") {
+        val board = Board.empty
+        assert( board.adjacent( Coordinate(2, 2), Coordinate(4, 4)) == false )
+        assert( board.adjacent( Coordinate(2, 2), Coordinate(1, 4)) == false )
+        assert( board.adjacent( Coordinate(2, 2), Coordinate(4, 1)) == false )
+      }
+
+    }
+
+    describe("#outOfBounds") {
+
+      it("succeeds if coordinate is inside board") {
+        val board = Board.empty
+        assert( board.outOfBounds(Coordinate(1, 1))  == false )
+        assert( board.outOfBounds(Coordinate(board.sizeX, board.sizeY))  == false )
+      }
+
+      it("fails if coordinate is outside the board") {
+        val board = Board.empty
+        assert( board.outOfBounds(Coordinate(0, 0))  == true )
+        assert( board.outOfBounds(Coordinate(1, 0))  == true )
+        assert( board.outOfBounds(Coordinate(0, 1))  == true )
+
+        assert( board.outOfBounds(Coordinate(board.sizeX + 1, board.sizeY))  == true )
+        assert( board.outOfBounds(Coordinate(board.sizeX, board.sizeY + 1))  == true )
+        assert( board.outOfBounds(Coordinate(board.sizeX + 1, board.sizeY + 1))  == true )
+      }
+
+    }
+
     describe("#empty") {
 
       it("is possible to create an empty board") {

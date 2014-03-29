f6d4f1be3bbd158806e81e7d27c4a6c704573a0e
Pedro Santos <donbonifacio@gmail.com>
2014-03-29 22:50:57 +0000

    Add split option to movement generator

--- src/main/scala/obb/generators/MovementGenerator.scala
+++ src/main/scala/obb/generators/MovementGenerator.scala
@@ -6,13 +6,13 @@ import obb.engine.actions._
 
 object MovementGenerator {
 
-  def run(turn : PlayerTurn, coordinate : Coordinate) = {
-    new MovementGenerator().run(turn, coordinate)
+  def run(turn : PlayerTurn, coordinate : Coordinate, split : Boolean = false) = {
+    new MovementGenerator(split).run(turn, coordinate)
   }
 
 }
 
-class MovementGenerator extends MoveGenerator {
+class MovementGenerator(split: Boolean = false) extends MoveGenerator {
 
   def process(turn : PlayerTurn, coordinate : Coordinate, element : Element) : List[PlayerTurn] = {
     val movementType = element.unit.movementType
@@ -22,14 +22,23 @@ class MovementGenerator extends MoveGenerator {
         val maybe = Coordinate(coordinate.x + x, coordinate.y + y)
         if( maybe != coordinate ) {
           if(movementType.movementPossible(turn.board, coordinate, maybe)) {
-            val action = MovementType.action(turn.board, coordinate, maybe, element.quantity)
-            movs ::= turn ~ action
+            eachSplitOption(element) { quantity =>
+              val action = MovementType.action(turn.board, coordinate, maybe, quantity)
+              movs ::= turn ~ action
+            }
           }
         }
       }
     }
 
     movs
+  }
+
+  def eachSplitOption(element : Element)(f : (Int) => Unit ) {
+    f(element.quantity)
+    if(split && element.quantity > 1) {
+      f(element.quantity / 2)
+    }
   }
 
 }

d73b30c192f4bced7a1e699198a844a5b13d6ce4
Pedro Santos <donbonifacio@gmail.com>
2014-04-15 14:32:43 +0100

    Refactor

--- src/main/scala/obb/evaluators/SimpleBoardEvaluator.scala
+++ src/main/scala/obb/evaluators/SimpleBoardEvaluator.scala
@@ -6,12 +6,10 @@ import obb.engine.{Board, Player}
 class SimpleBoardEvaluator extends BoardEvaluator {
 
   def evaluate(board : Board, player : Player) : Float = {
-    var sum = 0
-    board.eachElement{ element =>
+    board.elements.foldLeft(0) { (acc, element) =>
       val factor = if(element.player == player) 1 else -1
-      sum += (element.unit.value * element.quantity * factor)
+      acc + (element.unit.value * element.quantity * factor)
     }
-    sum
   }
 
 }
